75d44322191c7e8e650e29eef6ca012c8836828c
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-07-07 17:23:37 +0700

    edit underline

--- lib/widgets/link_text.dart
+++ lib/widgets/link_text.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class LinkText extends StatelessWidget {
+class LinkText extends StatefulWidget {
   final String text;
   final String url;
 
@@ -12,16 +12,28 @@ class LinkText extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _LinkTextState createState() => _LinkTextState();
+}
+
+class _LinkTextState extends State<LinkText> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        launch(url);
-      },
-      child: Text(
-        text,
-        style: const TextStyle(
-          decoration: TextDecoration.underline,
-          color: Colors.blue,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: InkWell(
+        onTap: () {
+          launch(widget.url);
+        },
+        child: Text(
+          widget.text,
+          style: TextStyle(
+            decoration:
+                _isHovered ? TextDecoration.underline : TextDecoration.none,
+            color: Colors.blue,
+          ),
         ),
       ),
     );
