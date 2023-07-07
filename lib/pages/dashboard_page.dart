46894686ca3dc2a8c1f4626750a0809a7907da69
@abdullah.muzaki <abdullah.muzaki@aruna.id>
2023-07-07 17:48:16 +0700

    fix warning

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -6,30 +6,7 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.email),
-            onPressed: () {
-              // Handle the action for icon1
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.email),
-            onPressed: () {
-              // Handle the action for icon2
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.email),
-            onPressed: () {
-              // Handle the action for icon3
-            },
-          ),
-        ],
-      ),
+      appBar: const Navbar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -50,4 +27,43 @@ class DashboardPage extends StatelessWidget {
       ),
     );
   }
+}
+
+class Navbar extends StatelessWidget implements PreferredSizeWidget {
+  const Navbar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.email),
+          onPressed: () {
+            // Handle the action for icon1
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.email),
+          onPressed: () {
+            // Handle the action for icon2
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.email),
+          onPressed: () {
+            // Handle the action for icon3
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
