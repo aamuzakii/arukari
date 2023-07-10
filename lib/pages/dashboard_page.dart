549c53030edf21771f2db49a94c2b48402e628e2
@abdullah.muzaki <aamuzakii@gmail.com>
2023-07-10 17:12:45 +0700

    extract sidebar

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,6 +1,6 @@
 import 'package:arukari/widgets/navbar.dart';
+import 'package:arukari/widgets/sidebar.dart';
 import 'package:flutter/material.dart';
-import '../widgets/icon_with_link.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -56,71 +56,6 @@ class RightSidebar extends StatelessWidget {
       width: 300,
       height: 488,
       color: Colors.white,
-    );
-  }
-}
-
-class LeftSidebar extends StatelessWidget {
-  const LeftSidebar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 488,
-      color: Colors.white,
-      child: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                children: const [
-                  Text("Quick Links"),
-                  IconWithLink(label: "My Info", icon: Icons.person),
-                  IconWithLink(
-                      label: "My Attendance Logs", icon: Icons.lock_clock),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                children: const [
-                  Text("Applications"),
-                  IconWithLink(label: "Forms", icon: Icons.email),
-                  IconWithLink(
-                      label: "Performance Review", icon: Icons.lock_clock),
-                  IconWithLink(label: "Insight", icon: Icons.lock_clock),
-                  IconWithLink(
-                      label: "Advance Recruitment", icon: Icons.lock_clock),
-                  IconWithLink(label: "Marketplace", icon: Icons.lock_clock),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                children: const [
-                  Text("Download Talenta Mobile"),
-                  IconWithLink(label: "App Store", icon: Icons.person),
-                  IconWithLink(label: "Google Play", icon: Icons.lock_clock),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/widgets/sidebar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../widgets/icon_with_link.dart';
+
+class LeftSidebar extends StatelessWidget {
+  const LeftSidebar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 488,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                children: const [
+                  Text("Quick Links"),
+                  IconWithLink(label: "My Info", icon: Icons.person),
+                  IconWithLink(
+                      label: "My Attendance Logs", icon: Icons.lock_clock),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                children: const [
+                  Text("Applications"),
+                  IconWithLink(label: "Forms", icon: Icons.email),
+                  IconWithLink(
+                      label: "Performance Review", icon: Icons.lock_clock),
+                  IconWithLink(label: "Insight", icon: Icons.lock_clock),
+                  IconWithLink(
+                      label: "Advance Recruitment", icon: Icons.lock_clock),
+                  IconWithLink(label: "Marketplace", icon: Icons.lock_clock),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                children: const [
+                  Text("Download Talenta Mobile"),
+                  IconWithLink(label: "App Store", icon: Icons.person),
+                  IconWithLink(label: "Google Play", icon: Icons.lock_clock),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
