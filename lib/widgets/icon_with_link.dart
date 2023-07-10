708a83bdf66edb52b3b4f97581174174953a81c8
@abdullah.muzaki <aamuzakii@gmail.com>
2023-07-10 16:43:59 +0700

    connect IconWithLink to LinkText widget

--- lib/widgets/icon_with_link.dart
+++ lib/widgets/icon_with_link.dart
@@ -1,16 +1,19 @@
+import 'package:arukari/widgets/link_text.dart';
 import 'package:flutter/material.dart';
 
 class IconWithLink extends StatelessWidget {
   final String label;
+  final String url;
   final IconData icon;
 
-  const IconWithLink({Key? key, required this.label, required this.icon})
+  const IconWithLink(
+      {Key? key, required this.label, required this.icon, required this.url})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [Icon(icon), Text(label)],
+      children: [Icon(icon), LinkText(text: label, url: url)],
     );
   }
 }
