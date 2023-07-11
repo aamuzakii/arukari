27cb37405fcf3ab59d162cf4141ee043b0c897a4
@abdullah.muzaki <aamuzakii@gmail.com>
2023-07-11 14:28:20 +0700

    add FractionallySizedBox 0.67

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -28,59 +28,62 @@ class DashboardPage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const LeftSidebar(),
-                  const SizedBox(width: 30),
-                  Column(
-                    children: [
-                      Container(
-                        color: Colors.purple,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 200,
-                              height: 200,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage(
-                                      "assets/images/banner-marketplace.png"),
-                                  fit: BoxFit.fitHeight, //
+              FractionallySizedBox(
+                widthFactor: 0.67,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const LeftSidebar(),
+                    const SizedBox(width: 30),
+                    Column(
+                      children: [
+                        Container(
+                          color: Colors.purple,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 200,
+                                height: 200,
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        "assets/images/banner-marketplace.png"),
+                                    fit: BoxFit.fitHeight, //
+                                  ),
                                 ),
                               ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.all(30),
-                              child: Column(children: const [
-                                BigText(
-                                  "Introducing Mekari Marketplace",
-                                  14,
-                                  color: Colors.white,
-                                ),
-                                BigText(
-                                  "Find essential add-ons for Talenta to drive your productivity",
-                                  18,
-                                  color: Colors.white,
-                                ),
-                              ]),
-                            ),
-                          ],
+                              Container(
+                                padding: const EdgeInsets.all(30),
+                                child: Column(children: const [
+                                  BigText(
+                                    "Introducing Mekari Marketplace",
+                                    14,
+                                    color: Colors.white,
+                                  ),
+                                  BigText(
+                                    "Find essential add-ons for Talenta to drive your productivity",
+                                    18,
+                                    color: Colors.white,
+                                  ),
+                                ]),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 8),
-                      Container(
-                        width: 600,
-                        height: 257,
-                        color: Colors.white,
-                        child: const MyTabbedWidget(),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 30),
-                  const RightSection(),
-                ],
+                        const SizedBox(height: 8),
+                        Container(
+                          width: 600,
+                          height: 257,
+                          color: Colors.white,
+                          child: const MyTabbedWidget(),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 30),
+                    const RightSection(),
+                  ],
+                ),
               )
             ],
           ),
