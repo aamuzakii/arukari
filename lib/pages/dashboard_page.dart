16b835a6ee2bfa7b80abdca2cdb82383de36a753
@abdullah.muzaki <aamuzakii@gmail.com>
2023-07-11 14:37:47 +0700

    add LimitedBox

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -57,48 +57,51 @@ class MiddleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          color: Colors.purple,
-          child: Row(
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/banner-marketplace.png"),
-                    fit: BoxFit.fitHeight, //
+    return LimitedBox(
+      maxWidth: 600,
+      child: Column(
+        children: [
+          Container(
+            color: Colors.purple,
+            child: Row(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/banner-marketplace.png"),
+                      fit: BoxFit.fitHeight, //
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(30),
-                child: Column(children: const [
-                  BigText(
-                    "Introducing Mekari Marketplace",
-                    14,
-                    color: Colors.white,
-                  ),
-                  BigText(
-                    "Find essential add-ons for Talenta to drive your productivity",
-                    18,
-                    color: Colors.white,
-                  ),
-                ]),
-              ),
-            ],
+                Container(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(children: const [
+                    BigText(
+                      "Introducing Mekari Marketplace",
+                      14,
+                      color: Colors.white,
+                    ),
+                    BigText(
+                      "Find essential add-ons for Talenta to drive your productivity",
+                      18,
+                      color: Colors.white,
+                    ),
+                  ]),
+                ),
+              ],
+            ),
           ),
-        ),
-        const SizedBox(height: 8),
-        Container(
-          width: 600,
-          height: 257,
-          color: Colors.white,
-          child: const MyTabbedWidget(),
-        ),
-      ],
+          const SizedBox(height: 8),
+          Container(
+            width: 600,
+            height: 257,
+            color: Colors.white,
+            child: const MyTabbedWidget(),
+          ),
+        ],
+      ),
     );
   }
 }
