c0b4fdebdc4b3255172900a58f4223adbfe90679
@abdullah.muzaki <aamuzakii@gmail.com>
2023-07-11 14:33:03 +0700

    Extract Middle section widget

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -33,55 +33,12 @@ class DashboardPage extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const LeftSidebar(),
-                    const SizedBox(width: 30),
-                    Column(
-                      children: [
-                        Container(
-                          color: Colors.purple,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 200,
-                                height: 200,
-                                decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                        "assets/images/banner-marketplace.png"),
-                                    fit: BoxFit.fitHeight, //
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.all(30),
-                                child: Column(children: const [
-                                  BigText(
-                                    "Introducing Mekari Marketplace",
-                                    14,
-                                    color: Colors.white,
-                                  ),
-                                  BigText(
-                                    "Find essential add-ons for Talenta to drive your productivity",
-                                    18,
-                                    color: Colors.white,
-                                  ),
-                                ]),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        Container(
-                          width: 600,
-                          height: 257,
-                          color: Colors.white,
-                          child: const MyTabbedWidget(),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(width: 30),
-                    const RightSection(),
+                  children: const [
+                    LeftSidebar(),
+                    SizedBox(width: 30),
+                    MiddleSection(),
+                    SizedBox(width: 30),
+                    RightSection(),
                   ],
                 ),
               )
@@ -89,6 +46,59 @@ class DashboardPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class MiddleSection extends StatelessWidget {
+  const MiddleSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.purple,
+          child: Row(
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/banner-marketplace.png"),
+                    fit: BoxFit.fitHeight, //
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(30),
+                child: Column(children: const [
+                  BigText(
+                    "Introducing Mekari Marketplace",
+                    14,
+                    color: Colors.white,
+                  ),
+                  BigText(
+                    "Find essential add-ons for Talenta to drive your productivity",
+                    18,
+                    color: Colors.white,
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          width: 600,
+          height: 257,
+          color: Colors.white,
+          child: const MyTabbedWidget(),
+        ),
+      ],
     );
   }
 }
