adc74b18b83bf21a4bd4f23a8a851a0e75f0e57c
@abdullah.muzaki <aamuzakii@gmail.com>
2023-07-17 11:14:00 +0700

    add google icon

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,5 +1,6 @@
 import 'package:arukari/helper/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ButtonList extends StatelessWidget {
   final List<ButtonConfig> buttons;
@@ -15,18 +16,34 @@ class ButtonList extends StatelessWidget {
           child: TextButton(
             onPressed: button.onPress,
             style: TextButton.styleFrom(
-                textStyle: const TextStyle(fontWeight: FontWeight.w300),
-                padding: EdgeInsets.zero,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                    side: const BorderSide(color: Color(0xffe2e2e2))),
-                minimumSize: const Size(double.infinity, 50),
-                backgroundColor: Colors.white,
-                primary: Colors.white),
-            child: Text(
-              button.label,
-              style: const TextStyle(
-                  color: Constants.darkBlueLogin, fontWeight: FontWeight.w400),
+              textStyle: const TextStyle(fontWeight: FontWeight.w300),
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+                side: const BorderSide(color: Color(0xffe2e2e2)),
+              ),
+              minimumSize: const Size(double.infinity, 50),
+              backgroundColor: Colors.white,
+              primary: Colors.white,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/svg/google.svg',
+                  width: 20,
+                  height: 20,
+                ),
+                const SizedBox(
+                    width: 8), // Adjust the spacing between the icon and text
+                Text(
+                  button.label,
+                  style: const TextStyle(
+                    color: Constants.darkBlueLogin,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
             ),
           ),
         );
