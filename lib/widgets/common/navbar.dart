d474fcf9f5ba818b3baaa7c513c4cf3529edca4c
@abdullah.muzaki <aamuzakii@gmail.com>
2023-08-11 13:29:29 +0700

    Revert "POC dropdown"
    This reverts commit 2c64f23d0ca8825deb08e6d2bb256e6d99410a7e.

--- lib/widgets/common/navbar.dart
+++ lib/widgets/common/navbar.dart
@@ -1,4 +1,3 @@
-import 'package:arukari/pages/foo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -41,7 +40,6 @@ class Navbar extends StatelessWidget implements PreferredSizeWidget {
             },
             color: Colors.grey),
         const Colorful(),
-        Sepaket(),
         Container(
           width: 25, // Adjust the width and height as needed
           height: 25,
