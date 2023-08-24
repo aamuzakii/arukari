dcc98b420608f2ad9358c9b8ea6d08edc37983aa
@abdullah.muzaki <aamuzakii@gmail.com>
2023-08-24 14:40:30 +0700

    get data from nest API

--- lib/pages/employee.dart
+++ lib/pages/employee.dart
@@ -2,15 +2,12 @@
 
 import 'package:arukari/helper/constants.dart';
 import 'package:arukari/widgets/big_text.dart';
-import 'package:arukari/widgets/icon_with_link.dart';
-import 'package:arukari/widgets/lucy.dart';
-import 'package:arukari/widgets/middle_section.dart';
 import 'package:arukari/widgets/common/navbar.dart';
 import 'package:arukari/widgets/searchbar.dart';
-import 'package:arukari/widgets/sidebar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../widgets/right_sidebar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class EmployeeTable extends StatelessWidget {
   const EmployeeTable({Key? key}) : super(key: key);
@@ -34,22 +31,52 @@ class Employee {
   final String position;
   final String email;
 
-  Employee(this.name, this.id, this.organization, this.position, this.email);
+  Employee({
+    required this.name,
+    required this.id,
+    required this.organization,
+    required this.position,
+    required this.email,
+  });
 }
 
-class Foo extends StatelessWidget {
-  final List<Employee> employees = [
-    Employee("Aam Khusnun Ni'am", "AI0082", "Quality Development",
-        "QC Coordinator Lampung", "aam@aruna.id"),
-    Employee(
-        "Abd Rijal",
-        "KM41044",
-        "Operations",
-        "Local Heroes Assistant Muara Pasir",
-        "abdul.rijal@koperasijasamaritim.id"),
-    Employee("Abdul Hakim", "AI0065", "Creative", "Designer Supervisor",
-        "abdulhakim@aruna.id"),
-  ];
+class Foo extends StatefulWidget {
+  const Foo({Key? key}) : super(key: key);
+
+  @override
+  _FooState createState() => _FooState();
+}
+
+class _FooState extends State<Foo> {
+  List<Employee> employees = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:3000/api/employees'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      setState(() {
+        employees = jsonData.map((data) {
+          return Employee(
+            name: data['name'],
+            id: data['id'],
+            organization: data['organization'],
+            position: data['position'],
+            email: data['email'],
+          );
+        }).toList();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,81 +94,79 @@ class Foo extends StatelessWidget {
         Container(
           color: Colors.white,
           width: double.maxFinite,
-          child: Container(
-            child: Column(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(4.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5.5),
-                          border: Border.all(
-                            color: Constants.greySeven,
-                            width: 1.0,
-                          ),
-                        ),
-                        child: Row(
-                          children: [
-                            IconWithLinkPurple(
-                                label: 'Directory',
-                                icon: 'assets/svg/column.svg'),
-                            IconWithLinkPurple(
-                                label: 'Org chart', icon: 'assets/svg/org.svg'),
-                          ],
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(4.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.5),
+                        border: Border.all(
+                          color: Constants.greySeven,
+                          width: 1.0,
                         ),
                       ),
-                      Expanded(
-                        child: Container(),
+                      child: Row(
+                        children: [
+                          IconWithLinkPurple(
+                              label: 'Directory',
+                              icon: 'assets/svg/column.svg'),
+                          IconWithLinkPurple(
+                              label: 'Org chart', icon: 'assets/svg/org.svg'),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: SvgPicture.asset(
-                          'assets/svg/column.svg',
-                          width: 20,
-                          height: 20,
-                        ),
+                    ),
+                    Expanded(
+                      child: Container(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: SvgPicture.asset(
+                        'assets/svg/column.svg',
+                        width: 20,
+                        height: 20,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: SvgPicture.asset(
-                          'assets/svg/insight.svg',
-                          width: 20,
-                          height: 20,
-                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: SvgPicture.asset(
+                        'assets/svg/insight.svg',
+                        width: 20,
+                        height: 20,
                       ),
-                      Expanded(child: SearchBar())
-                    ],
-                  ),
-                ),
-                DataTable(
-                  dataRowColor:
-                      MaterialStateColor.resolveWith((states) => Colors.white),
-                  headingRowColor: MaterialStateColor.resolveWith(
-                      (states) => Constants.f1f5f9),
-                  columns: [
-                    DataColumn(label: Text('Employee Name')),
-                    DataColumn(label: Text('Employee ID')),
-                    DataColumn(label: Text('Organization')),
-                    DataColumn(label: Text('Job Position')),
-                    DataColumn(label: Text('Email')),
+                    ),
+                    Expanded(child: SearchBar())
                   ],
-                  rows: employees.map((employee) {
-                    return DataRow(cells: [
-                      DataCell(Text(employee.name)),
-                      DataCell(Text(employee.id)),
-                      DataCell(Text(employee.organization)),
-                      DataCell(Text(employee.position)),
-                      DataCell(Text(employee.email)),
-                    ]);
-                  }).toList(),
                 ),
-                FooterPaginationControl()
-              ],
-            ),
+              ),
+              DataTable(
+                dataRowColor:
+                    MaterialStateColor.resolveWith((states) => Colors.white),
+                headingRowColor: MaterialStateColor.resolveWith(
+                    (states) => Constants.f1f5f9),
+                columns: [
+                  DataColumn(label: Text('Employee Name')),
+                  DataColumn(label: Text('Employee ID')),
+                  DataColumn(label: Text('Organization')),
+                  DataColumn(label: Text('Job Position')),
+                  DataColumn(label: Text('Email')),
+                ],
+                rows: employees.map((employee) {
+                  return DataRow(cells: [
+                    DataCell(Text(employee.name)),
+                    DataCell(Text(employee.id)),
+                    DataCell(Text(employee.organization)),
+                    DataCell(Text(employee.position)),
+                    DataCell(Text(employee.email)),
+                  ]);
+                }).toList(),
+              ),
+              FooterPaginationControl()
+            ],
           ),
         ),
       ],
