7738992d8e4980050930ce5cb7aafc66eb91fbc0
@abdullah.muzaki <aamuzakii@gmail.com>
2023-08-24 14:30:11 +0700

    success ListView

--- lib/pages/my_info_page.dart
+++ lib/pages/my_info_page.dart
@@ -5,9 +5,60 @@ import 'package:arukari/widgets/link_text.dart';
 import 'package:arukari/widgets/my_info/tabbed_widget.dart';
 import 'package:arukari/widgets/my_info/sidebar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class MyInfoPage extends StatelessWidget {
+class User {
+  final int id;
+  final String userName;
+  final String password;
+
+  User({
+    required this.id,
+    required this.userName,
+    required this.password,
+  });
+}
+
+class MyInfoPage extends StatefulWidget {
   const MyInfoPage({Key? key}) : super(key: key);
+
+  @override
+  _MyInfoPageState createState() => _MyInfoPageState();
+}
+
+class _MyInfoPageState extends State<MyInfoPage> {
+  List<User> users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http
+        .get(Uri.parse('https://fakerestapi.azurewebsites.net/api/v1/Users'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      final List<User> fetchedUsers = [];
+      for (var userData in data) {
+        final user = User(
+          id: userData['id'],
+          userName: userData['userName'],
+          password: userData['password'],
+        );
+        fetchedUsers.add(user);
+      }
+      setState(() {
+        users = fetchedUsers;
+      });
+    } else {
+      // Handle error
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +97,18 @@ class MyInfoPage extends StatelessWidget {
               ),
             ),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return ListTile(
+                  title: Text(user.userName),
+                  subtitle: Text(user.password),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
